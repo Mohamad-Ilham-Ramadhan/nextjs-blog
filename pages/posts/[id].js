52c4ce26921ad7e190cf2636b07c9d27b7e06e07
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-03-22 19:11:33 +0700

    first commit, mau deploy nigh

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
+  console.log("postData", postData);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}></Date>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  // Return all list of possible value for id
+  return {
+    paths: getAllPostIds(),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
